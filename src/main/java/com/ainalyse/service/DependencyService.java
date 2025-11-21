e80bf70767652c1eaa26f83e265fb3f0797bcccb
XeresArk <gauravevergreen.raj@gmail.com>
2025-11-21 14:36:05 +0530

    Updated dependency dir path

--- src/main/java/com/ainalyse/service/DependencyService.java
+++ src/main/java/com/ainalyse/service/DependencyService.java
@@ -1,5 +1,6 @@
 package com.ainalyse.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.ainalyse.controller.DependencyController.DependencyMapRequest;
@@ -9,13 +10,15 @@ import com.ainalyse.mapper.ProjectScanner;
 
 @Service
 public class DependencyService {
-    
+    @Value("${dependency.map.dir}")
+    private String dependencyMapDir;
+
     public String generateDependencyMapJson(DependencyMapRequest request) {
         try {
             System.out.println("Scanning: " + request.projectPath);
             ProjectScanner scanner = new ProjectScanner(request.projectName, request.projectPath);
             DependencyMap map = scanner.scan();
-            String outputFile = request.outputPath + "\\" + request.projectName + ".json";
+            String outputFile = dependencyMapDir + "\\" + request.projectName + ".json";
             JsonWriter.write(outputFile, map);
             return "Dependency map generated at: " + outputFile;
         } catch (Exception e) {
